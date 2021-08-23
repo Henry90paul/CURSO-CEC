4ce00b1c6c9dc9566f511291f10ea88ad411fd68
Henry90paul <89412373+Henry90paul@users.noreply.github.com>
2021-08-23 12:32:46 -0500

    Add files via upload

---
+++ taller1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 18 10:55:36 2021
+
+@author: CONTROL_1
+"""
+x = float
+y = float
+x = 1
+y = 3*(x**3)-2*(x**2)+3*x-1
+print (y)
+
+x = float
+y = float
+x = -1
+y = 3*(x**3)-2*(x**2)+3*x-1
+print (y)
+
+x = float
+y = float
+x = 0
+y = 3*(x**3)-2*(x**2)+3*x-1
+print (y)
+
